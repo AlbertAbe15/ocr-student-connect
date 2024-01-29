465cb1d0c69938f73818afde0715cb9f6ede1443
Albert Agung Andika <81175028+AlbertAbe15@users.noreply.github.com>
2024-01-29 21:38:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,5 @@ def ocr():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    # serve(app, host="0.0.0.0", port=5000)
+    app.run(debug=True)
